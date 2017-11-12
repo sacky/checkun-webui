268dd04109737f3db0a241706600cc96a5003375
Sacky <skyy0814@gmail.com>
2017-11-12 13:04:21 +0900

    Set selected-tab color & Add side bar

--- public/src/js/menubar.js
+++ public/src/js/menubar.js
@@ -3,5 +3,9 @@ $(document).ready(function(){
     var tabIndex = $(this).attr('data-tabIndex');
     $('.tab-area').removeClass('current');
     $('.tab-area[data-tabIndex="'+ tabIndex +'"]').addClass('current');
+
+    // クリックしたタブにクラスを追加
+    $('.tab-list li').removeClass('selected');
+    $(this).addClass('selected');
   });
 });
